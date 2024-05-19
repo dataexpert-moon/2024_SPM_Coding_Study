509f438c9be38c592563987d4391d315071a9791
Dal Min <81904802+dataexpert-moon@users.noreply.github.com>
2024-05-19 21:28:34 +0900

    [level 0] Title: 배열 만들기 4, Time: 6.53 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,18 @@
+def solution(arr):
+    stk = []
+    i = 0
+    
+    while (i < len(arr)):
+        
+        if not stk:
+            stk.append(arr[i])
+            i += 1
+            
+        elif stk and stk[len(stk) - 1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+            
+        elif stk and stk[len(stk) - 1] >= arr[i]:
+            stk.pop()
+
+    return stk
