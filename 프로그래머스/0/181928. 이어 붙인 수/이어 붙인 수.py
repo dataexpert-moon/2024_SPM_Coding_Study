d75ae67a2d86ebb9bf41105d16125ba49f428ef1
Dal Min <81904802+dataexpert-moon@users.noreply.github.com>
2024-05-19 20:21:45 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,17 @@
+def solution(num_list):
+    
+    odd = ''
+    even = ''
+    
+    for num in num_list:
+        if (num % 2 == 0):
+            odd += str(num)
+        else:
+            even += str(num)
+    
+    return int(odd) + int(even)
+        
+    
+    
+    
+    return answer
