14702b6ed4edcd5df9551c4f412bc40043a02c68
Dal Min <81904802+dataexpert-moon@users.noreply.github.com>
2024-05-19 20:22:12 +0900

    [level 0] Title: 수 조작하기 2, Time: 20.11 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,14 @@
+def solution(numLog):
+    answer = ''
+    
+    for idx in range(len(numLog) - 1):
+        if (numLog[idx + 1] - numLog[idx] == 1):
+            answer += 'w'
+        elif (numLog[idx + 1] - numLog[idx] == -1):
+            answer += 's'
+        elif (numLog[idx + 1] - numLog[idx] == 10):
+            answer += 'd'
+        elif (numLog[idx + 1] - numLog[idx] == -10):
+            answer += 'a'
+    
+    return answer
