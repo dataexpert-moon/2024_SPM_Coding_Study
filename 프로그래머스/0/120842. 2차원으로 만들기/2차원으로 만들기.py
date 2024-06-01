42327e3d4ffe3ab51a187da33c9b90879883e291
Dal Min <81904802+dataexpert-moon@users.noreply.github.com>
2024-06-01 20:02:37 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,13 +1,5 @@
 def solution(num_list, n):
     answer = []
-    
-    start = 0
-    
-    for _ in range(len(num_list) // n):
-        answer.append(num_list[start:start+n])
-        start += n
-    
-    if start != len(num_list):
-        answer.append(num_list[start:])
-    
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i+n])
     return answer
