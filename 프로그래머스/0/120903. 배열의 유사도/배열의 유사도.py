e854e1d2ba1e5fcf32e13cd7c278a531ba1a29fd
Dal Min <81904802+dataexpert-moon@users.noreply.github.com>
2024-06-01 19:05:15 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,9 +1,2 @@
 def solution(s1, s2):
-    answer = 0
-    
-    for char_s1 in s1:
-        for char_s2 in s2:
-            if char_s1 == char_s2:
-                answer += 1
-
-    return answer
+    return len(set(s1) & set(s2))
