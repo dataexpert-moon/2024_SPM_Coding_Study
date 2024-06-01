dad57283f744ce1ccb4fec7d3590f408f8f2200e
Dal Min <81904802+dataexpert-moon@users.noreply.github.com>
2024-06-02 02:29:06 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -0,0 +1,6 @@
+def solution(numlist, n):
+    numlist = [(abs(n-num), -num) for num in numlist]
+    # 첫 번째 요소를 기준으로 오름차순 정렬 and 두 번째 요소를 기준으로 내림차순 정렬
+    numlist.sort()
+    # 두 번쨰 요소만 반환
+    return [-num for _, num in numlist]
