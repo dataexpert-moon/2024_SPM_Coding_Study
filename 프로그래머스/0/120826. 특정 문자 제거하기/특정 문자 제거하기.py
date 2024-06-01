03c0ecc3e8ae1c65e31276db5a45605ac16e5de7
Dal Min <81904802+dataexpert-moon@users.noreply.github.com>
2024-06-01 18:55:37 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,9 +1,2 @@
 def solution(my_string, letter):
-    answer = ''
-    
-    for char in my_string:
-        if char == letter:
-            continue
-        answer += char
-        
-    return answer
+    return my_string.replace(letter, '')
